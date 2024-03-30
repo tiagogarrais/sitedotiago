1b2cbe5ec626eaa68fd53292e14470899a6f0337
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-03-30 17:12:34 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import { useSession, signIn, signOut } from "next-auth/react"
-import Link from 'next/link'
-
+import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
 
 export default function Home() {
-  const { data: session } = useSession()
-    return (
-      <>
-        <h1>Esta é a página inicial</h1>
-        <p><Link href='/acesso-com-login'>Clique aqui para acessar a área protegida</Link></p>
-        <p><Link href='/api/restrito'>Clique aqui para acessar a API protegida</Link></p>        
-      </>
-    )
-
+  const { data: session } = useSession();
+  return (
+    <>
+      <h1>Este é o site do Tiago</h1>
+      <p>Encontre aqui o que está procurando</p>
+      <p>
+        <Link href="/cidades/brejosanto">Brejo Santo</Link>
+      </p>
+    </>
+  );
 }
