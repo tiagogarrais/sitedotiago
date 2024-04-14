232855a6b6952725f4f79baefe3ce05247eca5c7
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-04-14 20:15:56 -0300

    Update [cidade].jsx

--- pages/cidades/[cidade].jsx
+++ pages/cidades/[cidade].jsx
@@ -5,5 +5,65 @@ export default function Cidades() {
   const router = useRouter();
   const cidade = router.query.cidade;
 
-  return cidade;
+  return (
+    <>
+      <h1>Serviços e informações úteis na cidade de Brejo Santo - CE</h1>
+
+      <div className="principal">
+        <h2>Profissionais autônomos</h2>
+        <h3>Motorista</h3>
+        <article>
+          <span>Junior Moral</span>
+          <span> - </span>
+          <span className="whatsApp">
+            <a target="_blank" href="https://wa.me/88988888888">
+              WhatsApp
+            </a>
+          </span>
+          <span> - </span>
+          <span className="Instagram">
+            <a target="_blank" href="https://www.instagram.com/juniormoral/">
+              Instagram
+            </a>
+          </span>
+        </article>
+
+        <h3>Encanadores</h3>
+        <div>
+          <article>
+            <span>João Silva</span>
+            <span> - </span>
+            <span className="whatsApp">
+              <a href="https://wa.me/88988888888">WhatsApp</a>
+            </span>
+          </article>
+        </div>
+
+        <div className="delivery-comida">
+          <h2>Pedir comida</h2>
+          <h3>Restaurantes</h3>
+          <article>
+            <div>
+              <span>Arena do Bode</span>
+              <span> - </span>
+              <span className="whatsApp">
+                <a href="https://wa.me/88999999999">WhatsApp</a>{" "}
+              </span>
+            </div>
+          </article>
+
+          <h3>Hamburgueria</h3>
+          <article>
+            <div>
+              <span>Hamburger Dogão</span>
+              <span> - </span>
+              <span className="whatsApp">
+                <a href="https://wa.me/88999999999">WhatsApp</a>{" "}
+              </span>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
 }
