a6a01d696aaa1d0a0ed26ba482c96e952203472e
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-04-17 13:55:48 -0300

    Update profissionais.jsx

--- components/profissionais.jsx
+++ components/profissionais.jsx
@@ -1,25 +1,45 @@
-export default function Profissionais({ nome, numeroWhatsapp, instagram }) {
+import { useState } from "react";
+
+export default function Profissionais({
+  id,
+  atividade,
+  nome,
+  numeroWhatsapp,
+  instagram,
+}) {
+  const [mostrarDetalhes, setMostrarDetalhes] = useState(false);
   const codigoArea = numeroWhatsapp.substring(0, 2);
   const primeiraParteTelefone = numeroWhatsapp.substring(2, 7);
   const segundaParteTelefone = numeroWhatsapp.substring(7, 11);
 
+  function handleClick() {
+    setMostrarDetalhes(!mostrarDetalhes); // Alternar o estado de exibição dos detalhes
+  }
+
   return (
-    <article>
-      <span>{nome} - </span>
-      <span>
-        ({codigoArea}) {primeiraParteTelefone}-{segundaParteTelefone} -{" "}
-      </span>
-      <span>
-        <a target="_blank" href={`https://wa.me/${numeroWhatsapp}`}>
-          WhatsApp
-        </a>
-      </span>
-      <span> - </span>
-      <span>
-        <a target="_blank" href={`https://www.instagram.com/${instagram}/`}>
-          Instagram
-        </a>
-      </span>
+    <article onClick={handleClick}>
+      <div className="basico">
+        <span>{atividade} - </span>
+        <span>{nome} - Mais detalhes</span>
+      </div>
+      {mostrarDetalhes && ( // Exibir os detalhes apenas se mostrarDetalhes for true
+        <div id={id} className="detalhes">
+          <span>
+            ({codigoArea}) {primeiraParteTelefone}-{segundaParteTelefone} -{" "}
+          </span>
+          <span>
+            <a target="_blank" href={`https://wa.me/${numeroWhatsapp}`}>
+              WhatsApp
+            </a>
+          </span>
+          <span> - </span>
+          <span>
+            <a target="_blank" href={`https://www.instagram.com/${instagram}/`}>
+              Instagram
+            </a>
+          </span>
+        </div>
+      )}
     </article>
   );
 }
