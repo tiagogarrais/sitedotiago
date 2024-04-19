65c60a8f3a2de4b9c2e1c4843379f3c2fa7c8995
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-04-19 03:51:11 -0300

    Update cabecalho.jsx

--- components/cabecalho.jsx
+++ components/cabecalho.jsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 export default function Cabecalho() {
   return (
-    <>
+    <div className="cabecalho">
+      <Image
+        src="/images/logomarca.jpeg"
+        width={75}
+        height={75}
+        alt="Logomarca do site do Tiago.com.br"
+      />
       <h1>SiteDoTiago.com.br</h1>
-    </>
+    </div>
   );
 }
