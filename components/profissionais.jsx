85e2077a319cb8b40dc8b03dc049e4c445140084
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-04-19 03:51:19 -0300

    Update profissionais.jsx

--- components/profissionais.jsx
+++ components/profissionais.jsx
@@ -17,30 +17,35 @@ export default function Profissionais({
   }
 
   return (
-    <article onClick={handleClick}>
-      <div className="basico">
-        <span>{atividade} - </span>
-        <span>{nome} - Mais detalhes</span>
-      </div>
-      {mostrarDetalhes && ( // Exibir os detalhes apenas se mostrarDetalhes for true
-        <div id={id} className="detalhes">
-          <p>
-            Telefone ({codigoArea}) {primeiraParteTelefone}-
-            {segundaParteTelefone}
-          </p>
-          <p>
-            <a target="_blank" href={`https://wa.me/${numeroWhatsapp}`}>
-              Chamar no WhatsApp
-            </a>
-          </p>
-
-          <p>
-            <a target="_blank" href={`https://www.instagram.com/${instagram}/`}>
-              Acessar Instagram
-            </a>
-          </p>
+    <>
+      <article onClick={handleClick}>
+        <div className="basico">
+          <span>{atividade} - </span>
+          <span>{nome} - Mais detalhes</span>
         </div>
-      )}
-    </article>
+        {mostrarDetalhes && ( // Exibir os detalhes apenas se mostrarDetalhes for true
+          <div id={id} className="detalhes">
+            <p>
+              Telefone ({codigoArea}) {primeiraParteTelefone}-
+              {segundaParteTelefone}
+            </p>
+            <p>
+              <a target="_blank" href={`https://wa.me/${numeroWhatsapp}`}>
+                Chamar no WhatsApp
+              </a>
+            </p>
+
+            <p>
+              <a
+                target="_blank"
+                href={`https://www.instagram.com/${instagram}/`}
+              >
+                Acessar Instagram
+              </a>
+            </p>
+          </div>
+        )}
+      </article>
+    </>
   );
 }
