c77389c6e877b21955eb1342542abe2e972a76dc
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-04-18 21:55:35 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,6 @@ export default function Home() {
   const { data: session } = useSession();
   return (
     <>
-      <h1>Este é o site do Tiago</h1>
-      <p>Encontre aqui o que está procurando</p>
       <p>
         <Link href="/cidades/brejosanto">Brejo Santo</Link>
       </p>
