71f1586353b66cb24cbaf9b0f8252e058d5eff22
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-04-20 01:54:26 -0300

    Update [cidade].jsx

--- pages/cidades/[cidade].jsx
+++ pages/cidades/[cidade].jsx
@@ -1,32 +1,48 @@
-import Link from "next/link";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Profissionais from "../../components/profissionais";
 import Formulario from "../../components/formulario";
-import Premium from "../../components/premium";
 
 export default function Cidades() {
   const router = useRouter();
   const cidade = router.query.cidade;
 
+  // Estado para armazenar os dados dos profissionais
+  const [profissionais, setProfissionais] = useState([]);
+
+  useEffect(() => {
+    // Função para buscar dados dos profissionais da API
+    const fetchData = async () => {
+      try {
+        // Faça sua chamada à API aqui e substitua a URL pela sua
+        const response = await fetch("/api/get-profissionais");
+        const data = await response.json();
+        setProfissionais(data);
+      } catch (error) {
+        console.error("Erro ao buscar dados dos profissionais:", error);
+      }
+    };
+
+    // Chamada da função para buscar os dados dos profissionais ao montar o componente
+    fetchData();
+  }, []); // Executa apenas uma vez ao montar o componente
+
   return (
     <div>
       <div className="principal">
         <h3>Empresas e profissionais cadastrados</h3>
-        <Profissionais
-          id="1"
-          nome="Junior Moral"
-          atividade="Motorista"
-          numeroWhatsapp="88955552222"
-          instagram="juniormoralmotorista"
-        />
-
-        <Profissionais
-          id="2"
-          nome="Zé da Topic"
-          atividade="Motorista"
-          numeroWhatsapp="88944447777"
-          instagram="zedatopic"
-        />
+        {/* Mapeamento dos dados dos profissionais */}
+        {profissionais.map((profissional) => (
+          <Profissionais
+            key={profissional.id} // Certifique-se de ter uma chave única para cada profissional
+            id={profissional.id}
+            nome={profissional.nome}
+            atividade={profissional.atividade}
+            cidade={profissional.cidade}
+            numeroWhatsapp={profissional.numeroWhatsapp}
+            instagram={profissional.instagram}
+          />
+        ))}
       </div>
 
       <Formulario />
