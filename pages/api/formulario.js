bbe116c25f494580ef9fe23076a98771381e06f8
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-04-20 04:44:35 -0300

    Update formulario.js

--- pages/api/formulario.js
+++ pages/api/formulario.js
@@ -5,9 +5,9 @@ const { MONGODB_URI, MONGODB_DB } = process.env;
 export default async function Formulario(req, res) {
   if (req.method === "POST") {
     // Verificar se os dados do formulário foram enviados corretamente
-    const { nome, profissao, telefone, instagram } = req.body;
+    const { nome, profissao, numeroWhatsapp, instagram } = req.body;
 
-    if (!nome || !profissao || !telefone || !instagram) {
+    if (!nome || !profissao || !numeroWhatsapp || !instagram) {
       return res
         .status(400)
         .json({ error: "Todos os campos do formulário são obrigatórios." });
@@ -25,7 +25,7 @@ export default async function Formulario(req, res) {
       await db.collection("profissionais").insertOne({
         nome,
         profissao,
-        telefone,
+        numeroWhatsapp,
         instagram,
       });
 
