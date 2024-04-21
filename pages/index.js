f6ce87cf4cccdec24c1a24a835025bea852ae687
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-04-21 02:32:30 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
+import { useEffect } from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 
 export default function Home() {
   const { data: session } = useSession();
-  return (
-    <>
-      <p>
-        <Link href="/cidades/brejosanto">Brejo Santo</Link>
-      </p>
-    </>
-  );
+
+  // Função para redirecionar para o link quando a página for carregada
+  useEffect(() => {
+    // Redireciona para o link
+    window.location.href = "/cidades/brejosanto";
+  }, []); // A dependência vazia faz com que o efeito seja executado apenas uma vez, quando a componente é montada
+
+  // Renderiza um componente vazio enquanto redireciona
+  return <>Estamos redirecionando para Brejo Santo</>;
 }
