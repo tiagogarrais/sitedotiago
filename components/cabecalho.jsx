5fcd69fc1272c97d107ced2fb638fa3741654f41
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-04-22 05:38:43 -0300

    Update cabecalho.jsx

--- components/cabecalho.jsx
+++ components/cabecalho.jsx
@@ -3,14 +3,23 @@ import Image from "next/image";
 export default function Cabecalho() {
   return (
     <div className="cabecalho">
-      <Image
-        src="/images/logomarca.jpeg"
-        width={75}
-        height={75}
-        alt="Logomarca do site do Tiago.com.br"
-      />
-      <a href="/" style={{ textDecoration: "none" }}>
-        <h1>SiteDoTiago.com.br</h1>
+      <a
+        href="/"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+        }}
+      >
+        {" "}
+        <Image
+          className="logomarca"
+          src="/images/logomarca.jpeg"
+          width={355}
+          height={355}
+          alt="Logomarca do SiteDoTiago.com.br"
+        />
       </a>
     </div>
   );
