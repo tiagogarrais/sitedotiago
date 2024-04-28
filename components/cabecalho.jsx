b3c27792a34585fb490bb82c4b708e22d3e9a83a
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-04-28 16:25:45 -0300

    Add contador

--- components/cabecalho.jsx
+++ components/cabecalho.jsx
@@ -1,8 +1,52 @@
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
+import Contador from "../components/contador";
 
 export default function Cabecalho() {
+  const [visitCount, setVisitCount] = useState(0);
+
+  useEffect(() => {
+    // Função para obter a contagem total de visitas
+    async function fetchVisitCount() {
+      try {
+        const response = await fetch("/api/contador");
+        if (response.ok) {
+          const data = await response.json();
+          setVisitCount(data.totalVisits);
+        } else {
+          console.error("Erro ao obter contagem de visitas:", response.status);
+        }
+      } catch (error) {
+        console.error("Erro ao obter contagem de visitas:", error);
+      }
+    }
+
+    fetchVisitCount(); // Chamada da função para obter a contagem total de visitas
+
+    // Função para registrar a visita
+    async function registerVisit() {
+      try {
+        await fetch("/api/contador", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            totalizador: visitCount + 1,
+            dataHora: new Date().toISOString(),
+          }),
+        });
+      } catch (error) {
+        console.error("Erro ao registrar visita:", error);
+      }
+    }
+
+    registerVisit(); // Chamada da função para registrar a visita
+  }, []); // Executa apenas uma vez, quando o componente é montado
+
   return (
     <div className="cabecalho">
+      <Contador visitCount={visitCount} />
       <a
         href="/"
         style={{
