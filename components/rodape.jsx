ad889c545fac51020c705aa255b66f7ac995f4a6
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-05-19 05:13:20 -0300

    Update rodape.jsx

--- components/rodape.jsx
+++ components/rodape.jsx
@@ -7,20 +7,20 @@ export default function Rodape() {
         <div>
           <h4>Sobre o site</h4>
           <p>
-            Site catálogo para reunir todas as empresas e profissionais
-            autônomos da cidade. Encontre serviços, produtos e contatos de forma
-            rápida e fácil. Promovendo a visibilidade e conectando consumidores
-            com empresas locais. Explore nossa ampla lista de fornecedores e
-            descubra tudo o que a comunidade tem a oferecer.
+            Este é um guia comercial no formato de site catálogo que reune
+            empresas e profissionais autônomos da cidade. Encontre serviços,
+            produtos e contatos de forma rápida e fácil. Promovendo a
+            visibilidade e conectando consumidores com empresas locais. Explore
+            nossa ampla lista e descubra tudo o que a comunidade tem a oferecer.
           </p>
           <p>
-            Além disso, através do nosso catálogo, você pode acessar diretamente
-            os perfis do WhatsApp, Instagram e os números de telefone das
-            empresas listadas. Simplificamos o processo de contato, permitindo
-            que você se conecte instantaneamente com os fornecedores que atendem
-            às suas necessidades. Seja para agendar um serviço, fazer uma
-            consulta ou obter mais informações, estamos aqui para facilitar sua
-            interação com as empresas locais.
+            Através do nosso catálogo, você pode acessar diretamente os perfis
+            do WhatsApp, Instagram e os números de telefone das empresas
+            listadas. Simplificamos o processo de contato, permitindo que você
+            se conecte instantaneamente com aqueles que atendem às suas
+            necessidades. Seja para agendar um serviço, fazer uma consulta ou
+            obter mais informações, estamos aqui para facilitar sua interação
+            com as empresas locais.
           </p>
           <p>
             Incentivamos a participação ativa da comunidade! Se você conhece uma
@@ -58,8 +58,10 @@ export default function Rodape() {
             facebook.com/admprogramador
           </a>
           <p id="creditos">
-            Desenvolvido por <span class="autor">Adm. Tiago Arrais</span> &copy;
-            2024
+            Desenvolvido por <span class="autor">Adm. Tiago Arrais.</span>{" "}
+            &copy;
+            <br />
+            Estamos online desde abril de 2024
           </p>
         </div>
       </footer>
