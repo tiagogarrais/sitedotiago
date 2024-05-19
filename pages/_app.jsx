c25f8fc5c98bd1ef177fb5b8b62f8cdf4d502e70
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-05-19 06:17:03 -0300

    Update _app.jsx
    Melhorias de SEO

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,5 @@
 import { SessionProvider } from "next-auth/react";
-import Head from "next/head"; // Importe o componente Head
+import Head from "next/head";
 import "../styles/globals.css";
 import Cabecalho from "../components/cabecalho";
 
@@ -13,8 +13,40 @@ export default function App({
         <title>
           Este é o guia comercial com o maior catálogo de empresas da cidade!
         </title>
+        <meta
+          name="description"
+          content="Catálogo com empresas e profissionais autônomos em Brejo Santo. Encontre os melhores serviços e produtos da cidade."
+        />
         <meta name="author" content="Tiago das Graças Arrais" />
-        <meta name="Catálogo com empresas e profissionais autônomos" />
+        <meta
+          name="keywords"
+          content="guia comercial, empresas, profissionais autônomos, Brejo Santo, serviços, produtos"
+        />
+
+        {/* Open Graph meta tags */}
+        <meta property="og:title" content="Guia Comercial de Brejo Santo" />
+        <meta
+          property="og:description"
+          content="Catálogo com empresas e profissionais autônomos em Brejo Santo. Encontre os melhores serviços e produtos da cidade."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.sitedotiago.com.br" />
+        <meta
+          property="og:image"
+          content="https://www.sitedotiago.com.br/images/logomarca.jpeg"
+        />
+
+        {/* Twitter meta tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Guia Comercial de Brejo Santo" />
+        <meta
+          name="twitter:description"
+          content="Catálogo com empresas e profissionais autônomos em Brejo Santo. Encontre os melhores serviços e produtos da cidade."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.sitedotiago.com.br/images/logomarca.jpeg"
+        />
       </Head>
       <Cabecalho />
       <Component {...pageProps} />
