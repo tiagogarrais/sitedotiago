c11f99e7129e9ce3c6153daf5974fc092e513342
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-05-31 11:43:30 -0300

    Create anuncie-aqui.jsx

---
+++ pages/cidades/anuncie-aqui/index.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+export default function AnuncieAqui() {
+  return (
+    <div>
+      <h2>Aumente sua visibilidade!</h2>
+      <p className="centralizada">
+        Anuncie no nosso guia comercial e destaque seu negócio.
+      </p>
+      <h2>Vantagens:</h2>
+      <ul>
+        <li>
+          <strong>Alcance Direto:</strong> Consumidores locais interessados nos
+          seus serviços.
+        </li>
+        <li>
+          <strong>Exclusividade:</strong> Apenas um banner por categoria.
+        </li>
+
+        <li>
+          <strong>Conexão Fácil:</strong> Contato direto via WhatsApp.
+        </li>
+
+        <li>
+          <strong>Visibilidade Contínua:</strong> Garantia de 12 meses de
+          exposição.
+        </li>
+        <li>
+          <strong>Baixo Custo:</strong> Apenas R$ 149,90 por um ano inteiro.
+        </li>
+      </ul>
+      <h2>Detalhes:</h2>
+      <ul>
+        <li>
+          <strong>Preço:</strong> R$ 149,90/ano.
+        </li>
+        <li>
+          <strong>Tamanho:</strong> 750 x 75 pixels.
+        </li>
+        <li>
+          <strong>Duração:</strong> 12 meses.
+        </li>
+      </ul>
+      <h2>Entre em contato agora!</h2>
+      <button className="botao centralizada">
+        <a href="https://wa.me/5588997230866">
+          Clique aqui para entrar em contato pelo WhatsApp
+        </a>
+      </button>
+      <h3>Mais informações</h3>
+      <p>
+        Após o pagamento você precisa enviar a sua arte no formato jpeg com
+        tamanho de 750x75 pixels. Nós não confeccionamos a arte. O período de 1
+        ano se inicia à partir do recebimento da arte e publicação no site. Além
+        do banner podemos entrar em contato para a criação de campanhas
+        publicitárias conjuntas sem custos adicionais.
+      </p>
+
+      <p>
+        Estamos construindo o maior catálogo de empresas e profissionais da
+        cidade. Junte-se a nós!
+      </p>
+    </div>
+  );
+}
