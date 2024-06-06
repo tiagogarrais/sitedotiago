d588511e5a12029930f7a77d58402729d6d520d7
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-06-05 21:36:51 -0300

    Update profissionais.jsx
    Removendo função premium

--- components/profissionais.jsx
+++ components/profissionais.jsx
@@ -13,65 +13,16 @@ export default function Profissionais({
   const primeiraParteTelefone = whatsApp ? whatsApp.substring(2, 7) : "";
   const segundaParteTelefone = whatsApp ? whatsApp.substring(7, 11) : "";
 
-  const tamanhoIcone = 22;
+  const tamanhoIcone = 30;
 
   return (
     <>
       <article>
         <div className="nome">{nome}</div>
-        {premium ? (
-          // Exibir informações premium (WhatsApp, Instagram e telefone)
-          <div className="premium">
-            {whatsApp && (
-              <>
-                <a
-                  target="_blank"
-                  href={`tel:${codigoArea}${primeiraParteTelefone}${segundaParteTelefone}`}
-                >
-                  <button>
-                    <Image
-                      src="/images/telefone.png"
-                      width={tamanhoIcone}
-                      height={tamanhoIcone}
-                      alt="Fazer uma ligação telefônica"
-                      role="img"
-                    ></Image>
-                  </button>
-                </a>
-                <a target="_blank" href={`https://wa.me/+55${whatsApp}`}>
-                  <button>
-                    <Image
-                      src="/images/logo-whatsapp.webp"
-                      width={tamanhoIcone}
-                      height={tamanhoIcone}
-                      alt="Mandar mensagem no WhatsApp"
-                      role="img"
-                    ></Image>
-                  </button>
-                </a>
-              </>
-            )}
-            {instagram && (
-              <a
-                target="_blank"
-                href={`https://www.instagram.com/${instagram}/`}
-              >
-                <button>
-                  <Image
-                    src="/images/logo-instagram.webp"
-                    width={tamanhoIcone}
-                    height={tamanhoIcone}
-                    alt="Acessar o Instagram"
-                    role="img"
-                  />
-                </button>
-              </a>
-            )}
-          </div>
-        ) : (
-          // Exibir apenas o telefone para não premium
-          <div className="basico">
-            {whatsApp && (
+
+        <div className="premium">
+          {whatsApp && (
+            <>
               <a
                 target="_blank"
                 href={`tel:${codigoArea}${primeiraParteTelefone}${segundaParteTelefone}`}
@@ -85,10 +36,35 @@ export default function Profissionais({
                     role="img"
                   ></Image>
                 </button>
-              </a>
-            )}
-          </div>
-        )}
+              </a>{" "}
+              <a target="_blank" href={`https://wa.me/+55${whatsApp}`}>
+                <button>
+                  <Image
+                    src="/images/logo-whatsapp.webp"
+                    width={tamanhoIcone}
+                    height={tamanhoIcone}
+                    alt="Mandar mensagem no WhatsApp"
+                    role="img"
+                  ></Image>
+                </button>
+              </a>{" "}
+            </>
+          )}
+
+          {instagram && (
+            <a target="_blank" href={`https://www.instagram.com/${instagram}/`}>
+              <button>
+                <Image
+                  src="/images/logo-instagram.webp"
+                  width={tamanhoIcone}
+                  height={tamanhoIcone}
+                  alt="Acessar o Instagram"
+                  role="img"
+                />
+              </button>
+            </a>
+          )}
+        </div>
       </article>
     </>
   );
